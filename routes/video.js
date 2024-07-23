6fc7f8c361952cb089f8c5316503eafaacc4592f
mickey-vee <mvoong27@gmail.com>
2024-07-22 23:00:12 -0400

    Changes to get video details get request to get the correct details

--- routes/video.js
+++ routes/video.js
@@ -3,14 +3,22 @@ import fs from "fs";
 
 const router = express.Router();
 
+//get video data
 router.get("/", (req, res) => {
   const videoData = fs.readFileSync("./data/video.json");
   res.json(JSON.parse(videoData));
 });
 
+//get all video/details data
 router.get("/:id", (req, res) => {
-  const videoData = fs.readFileSync("./data/videoData.json");
-  res.json(JSON.parse(videoData));
+  const { id } = req.params;
+  try {
+    const parsedData = JSON.parse(fs.readFileSync("./data/videoData.json"));
+    const video = parsedData.find((video) => video.id === id);
+    res.json(video);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 export default router;
